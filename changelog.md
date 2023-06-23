ad73a8e742b1ce86ed0c4b9cc7c46625a545953d
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-06-23 05:51:12 +0530

    Add file tree

---
+++ changelog.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## 3.0.0 (LEXX) [Latest]
+- Move back to CodeMirror because Monaco is 45 tonnes
+- Move to Sandpack because it's a wrapper on CodeMirror
+
+
+## 2.0.0 (AMOSCode)
+- Monaco Editor on a Svelte REPL with Files
+
+
+## 1.0.0 (CM2Frame)
+- A basic CodeMirror 4 to iframe renderer
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Modes from "./modes";
 import { useState } from "react";
-import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackPreview
+} from "@codesandbox/sandpack-react";
 import { atomDark } from "@codesandbox/sandpack-themes";
 
 import options from "./options.json";
@@ -18,19 +24,35 @@ function App () {
       } );
     // FILES
     if ( type === "files" ) {
-      if ( files )
-        setMode( { template: value, files } );
+      let mode = {}
+      if ( files ) mode = { template: value, files }
       else {
-        const inherited = options.find( ( s ) =>
+        const i = options.find( s =>
           s.value === value
         );
-        setMode( {
-          template: inherited.template,
-          files: inherited.files
-        } );
+        mode = { template: i.template, files: i.files };
       }
+      setMode( mode );
     };
   };
+
+  const addFile = () => {
+    let tempMode = mode;
+    const name = prompt( "Create File" );
+    const ext = name.split( "." ).pop();
+    if ( !( name || ext ) ) return 0;
+
+    // tempMode.files[ '/' + name ] = "Hi";
+    console.log( tempMode );
+    setMode( {
+      template: tempMode.template,
+      files: {
+        ...tempMode.files,
+        [ '/' + name ]: ""
+      }
+    } );
+  };
+
 
   window.onmessage = ( { data: {
     type, value, files
@@ -40,20 +62,41 @@ function App () {
 
   const navbar = (
     <div className="f j-bw ">
-      <div className="m10 title">LEXX</div>
-      <Modes onload={starter} onChange={handleOptionChange} />
+      <div className="m10 title">
+        LEXX
+        <svg
+          className="ptr p2 rx5"
+          viewBox="0 0 32 32"
+          width="32"
+          height="32"
+          style={{
+            height: "24px",
+            width: "24px",
+            margin: "5px 0 5px 7px",
+            verticalAlign: "text-top",
+            background: "#445",
+            strokeWidth: "1",
+          }}
+          onClick={addFile}
+        >
+          <path d="M16 2 L16 30 M2 16 L30 16" stroke="#fff" />
+        </svg>
+      </div>
+      <Modes
+        onload={starter}
+        onChange={handleOptionChange}
+      />
     </div>
   )
 
   return (
     <>
       {window.isTop && navbar}
-      <Sandpack
-        theme={atomDark}
+      <SandpackProvider
         {...mode}
+        theme={atomDark}
         options={{
           showInlineErrors: true,
-          showNavigator: true,
           showReadOnly: false,
           showLineNumbers: true,
           showConsoleButton: true,
@@ -61,7 +104,13 @@ function App () {
           closableTabs: true,
           wrapContent: true,
         }}
-      />
+      >
+        <SandpackLayout>
+          <SandpackFileExplorer />
+          <SandpackCodeEditor closableTabs />
+          <SandpackPreview />
+        </SandpackLayout>
+      </SandpackProvider>
     </>
   );
 };
